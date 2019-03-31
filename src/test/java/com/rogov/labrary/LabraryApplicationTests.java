03cd511f926f466763ef69ed9d159a4e630d871d
AlexandrRogov <rohaualexandr@gmail.com>
2019-03-31 10:29:53 +0300

    First Commit

---
+++ src/test/java/com/rogov/labrary/LabraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rogov.labrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabraryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
